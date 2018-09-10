f37820dd5936e75209bac737632f6ddf58058758
wangbin <wangbin10@TECH.BITAUTO.COM>
2018-09-10 18:24:39 +0800

    update Kruskal.java

--- java-algorithm/Kruskal.java
+++ java-algorithm/Kruskal.java
@@ -10,7 +10,11 @@ import static algorithm.Prim.createNetWork;
  * 使用Kruskal算法生成最小生成树，以遍历所有连通顶点的边作为主要途径
  * 关键点在于：
  * 1.所有边按照权重从小到大排序
- * 2.已经连通的点，即便再遇到连通的边也不再计入最小连通集合
+ * 2.涉及到一个查并集的问题：
+ * （1）：需要考虑一条新的边加入到连通集时，是否会形成圈；
+ * （2）：判断的方法就是把已经连通的点集看做是一棵树，他们拥有相同的根节点，即parent;
+ * （3）：当我们想将一条边加入新连通集时，首先判断他们是否已经在一棵树上，方法就是find他们是否有相同的parent；
+ * （4）：如果没有，说明他们尚未连通，则把他们加入到连通点集中，并将两个节点所在的树合并成同一颗树，即union操作；
  * 3.重复上述过程，直到所有点都被连通
  */
 public class Kruskal {
@@ -18,15 +22,54 @@ public class Kruskal {
         NetWork netWork = createNetWork();
         List<Side> sides = getSides(netWork);
         Collections.sort(sides);
-        getMiniSpanTree(netWork, sides);
+        List<Integer> parent = new ArrayList<>();
+        Integer vertexNum = netWork.getVertexNum();
+        /**将每个节点的父节点初始化为节点本身*/
+        for (int i = 0; i < vertexNum; i++) {
+            parent.add(i, i);
+        }
+        getMiniSpanTree(netWork, parent, sides);
+
     }
 
-    private static void getMiniSpanTree(NetWork netWork, List<Side> sides) {
+    /**
+     * 查找某个节点的根节点，如果两个节点的根节点相同，说明他们在同一颗树上，也即他们是已经连通的
+     */
+    private static int findParent(NetWork netWork, List<Integer> parent, String x) {
+        for (int i = 0; i < netWork.getVertexNum(); i++) {
+            if (x.equals(netWork.getVertex()[i])) {
+                return parent.get(i);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 合并两个点所在的树，即把所有和两个点相同父节点的节点统一父节点
+     */
+    private static void union(NetWork netWork, List<Integer> parent, String x, String y) {
+        int m = findParent(netWork, parent, x);
+        int n = findParent(netWork, parent, y);
+
+        if (m != n) {
+            int newParent = m > n ? n : m;
+            for (int i = 0; i < parent.size(); i++) {
+                if (parent.get(i) == m || parent.get(i) == n) {
+                    parent.set(i, newParent);
+                }
+            }
+        }
+    }
+
+    private static void getMiniSpanTree(NetWork netWork, List<Integer> parent, List<Side> sides) {
         Set<String> vertex = new HashSet<>();
         while (vertex.size() != netWork.getVertexNum()) {
             for (Side side : sides) {
-                if (!(vertex.contains(side.getStart()) && vertex.contains(side.getEnd()))) {
+                String start = side.getStart();
+                String end = side.getEnd();
+                if (findParent(netWork, parent, start) != findParent(netWork, parent, end)) {
                     System.out.println(side.getStart() + "->" + side.getEnd());
+                    union(netWork, parent, start, end);
                     vertex.add(side.getStart());
                     vertex.add(side.getEnd());
                 }
